71067741c8a69bc508594bf3e72f479557feccb1
Jacob Kiesel <jacob.kiesel@infiniteathlete.ai>
2024-05-06 17:03:32 -0600

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,157 @@
+use std::{
+    any::Any, future::Future, marker::PhantomData, sync::{atomic::{AtomicUsize, Ordering}, Arc}
+};
+
+pub trait FutureInheritTaskLocal: Future {
+    fn inherit_task_local(self) -> impl Future<Output = Self::Output> + Send + Sync + 'static;
+}
+
+impl<F> FutureInheritTaskLocal for F
+where
+    F: Future + Send + Sync + 'static,
+{
+    fn inherit_task_local(self) -> impl Future<Output = Self::Output> + Send + Sync + 'static {
+        let mut this = Some(self); // Only one of the two paths will execute, but the borrow checker doesn't know that.
+        INHERITABLE_TASK_LOCALS
+            .try_with(|task_locals| {
+                let new_task_locals = task_locals.clone();
+                INHERITABLE_TASK_LOCALS.scope(new_task_locals, this.take().unwrap())
+            })
+            .unwrap_or_else(|_| INHERITABLE_TASK_LOCALS.scope(vec![None; NEXT_KEY.load(Ordering::Relaxed)], this.take().unwrap()))
+    }
+}
+
+tokio::task_local! {
+    // FIXME: This would be more performant if it were an array accessed by integer indexes instead. Sadly I couldn't
+    // find a way to automatically generate these indexes at compile time.
+    static INHERITABLE_TASK_LOCALS: Vec<Option<Arc<dyn Any + Send + Sync + 'static>>>
+}
+
+pub struct InheritableLocalKey<T: Clone + 'static> {
+    // FIXME: This would be more performant of it were some kind of unique integer determined at compile time.
+    // An earlier version of this attempted to abuse TypeIds of unit structs to make this happen, but it wasn't
+    // possible to make the unit structs have unique identifiers without employing a proc-macro.
+    #[doc(hidden)]
+    pub key: usize,
+    #[doc(hidden)]
+    pub _phantom: PhantomData<T>,
+}
+
+impl<T: Send + Sync + Clone> InheritableLocalKey<T> {
+    pub fn scope<F>(&'static self, value: T, f: F) -> impl Future<Output = F::Output>
+    where
+        F: Future,
+    {
+        let mut value = Some((value, f));
+        INHERITABLE_TASK_LOCALS
+            .try_with(|task_locals| {
+                let (value, f) = value.take().unwrap();
+                let mut new_task_locals = task_locals.clone();
+                new_task_locals[self.key] = Some(Arc::new(value) as Arc<_>);
+                INHERITABLE_TASK_LOCALS.scope(new_task_locals, f)
+            })
+            .unwrap_or_else(|_| {
+                let (value, f) = value.take().unwrap();
+                let mut new_task_locals = vec![None; NEXT_KEY.load(Ordering::Relaxed)];
+                new_task_locals[self.key] = Some(Arc::new(value) as Arc<_>);
+                INHERITABLE_TASK_LOCALS.scope(new_task_locals, f)
+            })
+    }
+
+    pub fn sync_scope<F, R>(&'static self, value: T, f: F) -> R
+    where
+        F: FnOnce() -> R,
+    {
+        let mut value = Some(value);
+        let new_task_locals = INHERITABLE_TASK_LOCALS
+            .try_with(|task_locals| {
+                let value = value.take().unwrap();
+                let mut new_task_locals = task_locals.clone();
+                new_task_locals[self.key] = Some(Arc::new(value) as Arc<_>);
+                new_task_locals
+            })
+            .unwrap_or_else(|_| {
+                let value = value.take().unwrap();
+                let mut new_task_locals = Vec::new();
+                new_task_locals[self.key] = Some(Arc::new(value) as Arc<_>);
+                new_task_locals
+            });
+        INHERITABLE_TASK_LOCALS.sync_scope(new_task_locals, f)
+    }
+
+    pub fn with<F, R>(&'static self, f: F) -> R
+    where
+        F: FnOnce(&T) -> R,
+    {
+        INHERITABLE_TASK_LOCALS.with(|task_locals| {
+            let v = task_locals
+                .get(self.key)
+                .expect("task local vec was the wrong length, this is a tokio-inherit-task-local bug")
+                .as_ref()
+                .expect("inheritable task local was not defined");
+            (f)(v
+                .downcast_ref::<T>()
+                .expect("internal was not of correct type, this is a tokio-inherit-task-local bug"))
+        })
+    }
+
+    pub fn try_with<F, R>(&'static self, f: F) -> Result<R, InheritableAccessError>
+    where
+        F: FnOnce(&T) -> R,
+    {
+        let r = INHERITABLE_TASK_LOCALS.try_with(|task_locals| {
+            let v = task_locals
+                .get(self.key)
+                .expect("task local vec was the wrong length, this is a tokio-inherit-task-local bug")
+                .as_ref()
+                .ok_or(InheritableAccessError::NotInHashmap)?;
+            Result::<_, InheritableAccessError>::Ok((f)(v.downcast_ref::<T>().expect(
+                "internal was not of correct type, this is a tokio-inherit-task-local bug",
+            )))
+        });
+        match r {
+            Ok(Ok(v)) => Ok(v),
+            Ok(Err(e)) => Err(e),
+            Err(_) => Err(InheritableAccessError::NotInTokio),
+        }
+    }
+}
+
+pub enum InheritableAccessError {
+    NotInHashmap,
+    NotInTokio,
+}
+
+#[macro_export]
+macro_rules! inheritable_task_local {
+    // empty (base case for the recursion)
+   () => {};
+
+   ($(#[$attr:meta])* $vis:vis static $name:ident: $t:ty; $($rest:tt)*) => {
+       $crate::__inheritable_task_local_inner!($(#[$attr])* $vis $name, $t);
+       $crate::inheritable_task_local!($($rest)*);
+   };
+
+   ($(#[$attr:meta])* $vis:vis static $name:ident: $t:ty) => {
+       $crate::__inheritable_task_local_inner!($(#[$attr])* $vis $name, $t);
+   }
+}
+
+#[doc(hidden)]
+#[macro_export]
+macro_rules! __inheritable_task_local_inner {
+   ($(#[$attr:meta])* $vis:vis $name:ident, $t:ty) => {
+       $(#[$attr])*
+       #[$crate::ctor::ctor]
+       $vis static $name: $crate::InheritableLocalKey<$t> = $crate::InheritableLocalKey {
+            key: $crate::NEXT_KEY.fetch_add(1, ::std::sync::atomic::Ordering::Relaxed),
+            _phantom: ::std::marker::PhantomData,
+       };
+   };
+}
+
+#[doc(hidden)]
+pub static NEXT_KEY: AtomicUsize = AtomicUsize::new(0);
+
+#[doc(hidden)]
+pub use ctor;
---
+++ tests/full.rs
@@ -0,0 +1,67 @@
+use tokio_inherit_task_local::{inheritable_task_local, FutureInheritTaskLocal};
+
+inheritable_task_local! {
+    pub static TEST_VALUE: u32;
+    pub static ANOTHER_TEST_VALUE: u32;
+}
+
+#[tokio::test]
+async fn basic() {
+    let out = TEST_VALUE.scope(5, async { TEST_VALUE.with(|&v| v) }).await;
+    assert_eq!(out, 5);
+}
+
+#[tokio::test]
+async fn basic_inherit() {
+    let out = TEST_VALUE
+        .scope(5, async {
+            tokio::spawn(async { TEST_VALUE.with(|&v| v) }.inherit_task_local()).await
+        })
+        .await
+        .unwrap();
+    assert_eq!(out, 5);
+}
+
+#[tokio::test]
+async fn inherit_repeatedly() {
+    let out = TEST_VALUE
+        .scope(5, async {
+            tokio::spawn(async {
+                tokio::spawn(async { TEST_VALUE.with(|&v| v) }.inherit_task_local())
+            }.inherit_task_local()).await
+        })
+        .await
+        .unwrap()
+        .await
+        .unwrap();
+    assert_eq!(out, 5);
+}
+
+#[tokio::test]
+#[should_panic]
+async fn not_inherited_if_future_not_wrapped() {
+    let out = TEST_VALUE
+        .scope(5, async {
+            tokio::spawn(async { TEST_VALUE.with(|&v| v) }).await
+        })
+        .await
+        .unwrap();
+    assert_eq!(out, 5);
+}
+
+
+#[tokio::test]
+#[should_panic]
+async fn not_inherited_repeatedly_if_chain_broken() {
+    let out = TEST_VALUE
+        .scope(5, async {
+            tokio::spawn(async {
+                tokio::spawn(async { TEST_VALUE.with(|&v| v) }.inherit_task_local())
+            }).await
+        })
+        .await
+        .unwrap()
+        .await
+        .unwrap();
+    assert_eq!(out, 5);
+}
